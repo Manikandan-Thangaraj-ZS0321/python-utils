d1359795efa1ee5d47019f23a3f6fb00df1df6c5
Manikandan-tm <manikandan.tm@zucisystems.com>
2024-02-08 00:18:54 +0530

    api changes for the multipart file download

--- app/multipartUtil.py
+++ app/multipartUtil.py
@@ -37,6 +37,8 @@ async def create_papers(file: UploadFile = File(...), outputDir: str = Query(...
 async def do_upload_file(filepath: str):
     try:
         file_path = Path(filepath)
+        if not file_path.exists():
+            raise FileNotFoundError(f"File not found at path: {file_path}")
         filename = os.path.basename(file_path)
         logger.info({"message": "File uploaded"})
 
@@ -48,9 +50,9 @@ async def do_upload_file(filepath: str):
         return StreamingResponse(file_iterator(file_path), media_type="application/octet-stream",
                                  headers={"Content-Disposition": f"attachment; filename={filename}"})
 
-    except Exception as e:
-        logger.error(f"Error in uploading multipart file {filepath} with exception {e}")
-        return {"error": str(e)}
+    except Exception as ex:
+        logger.error(f"Error in uploading multipart file {filepath} with exception {ex}")
+        raise ex
 
 if __name__ == "__main__":
     import uvicorn

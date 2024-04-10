f13cf3a10ceb4d47136cb76604f6c8509702abb8
dineshkumar.anandan@zucisystems.com <dineshkumar.anandan@zucisystems.com>
2024-04-10 13:20:55 +0530

    file delete api commit-1

--- app/multipartUtil.py
+++ app/multipartUtil.py
@@ -1,8 +1,9 @@
+import gc
 import os
 from pathlib import Path
 
-from fastapi import FastAPI, File, UploadFile, Query
-from fastapi.responses import StreamingResponse
+from fastapi import FastAPI, File, UploadFile, Query, HTTPException
+from fastapi.responses import StreamingResponse, JSONResponse
 
 from app.connection import getCommonConfig
 from app.loggers import logger
@@ -12,26 +13,23 @@ CONFIG = getCommonConfig()
 
 
 @app.post("/multipart-upload")
-async def create_papers(file: UploadFile = File(...), outputDir: str = Query(...)):
+async def create_papers(file: UploadFile = File(...), output_dir: str = Query(...)):
     try:
-        if not os.path.exists(outputDir):
-            os.makedirs(outputDir, exist_ok=True)
-
-        output_file = os.path.join(outputDir, file.filename)
-
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir, exist_ok=True)
+        output_file = os.path.join(output_dir, file.filename)
         if os.path.exists(output_file):
             logger.info("File {} already exists".format(file.filename))
             return {"message": "File already downloaded", "filename": file.filename}
-
         with open(output_file, "wb") as f:
             f.write(file.file.read())
-
-        logger.info("File {} downloaded successfully".format(file.filename))
+        logger.info("File {} downloaded successfully".format(file.filename), exc_info=True)
         return {"message": "File downloaded successfully", "filename": file.filename}
-
     except Exception as e:
-        logger.error("Error in downloading multipart file {} with exception {}".format(file.filename, e))
+        logger.error("Error in downloading multipart file {} with exception {}".format(file.filename, e), exc_info=True)
         return {"error": str(e)}
+    finally:
+        gc.collect()
 
 
 @app.post("/multipart-download")
@@ -41,19 +39,38 @@ async def do_upload_file(filepath: str):
         if not file_path.exists():
             raise FileNotFoundError(f"File not found at path: {file_path}")
         filename = os.path.basename(file_path)
-        logger.info({"message": "File uploaded"})
+        logger.info({"message": "File uploaded"}, exc_info=True)
 
-        def file_iterator(filepath):
-            with open(filepath, "rb") as file:
+        def file_iterator(file_p):
+            with open(file_p, "rb") as file:
                 while chunk := file.read(65536):
                     yield chunk
 
         return StreamingResponse(file_iterator(file_path), media_type="application/octet-stream",
                                  headers={"Content-Disposition": f"attachment; filename={filename}"})
-
     except Exception as ex:
-        logger.error(f"Error in uploading multipart file {filepath} with exception {ex}")
+        logger.error(f"Error in uploading multipart file {filepath} with exception {ex}", exc_info=True)
         raise ex
+    finally:
+        gc.collect()
+
+
+@app.post("/delete-file")
+async def delete_files(filepath: str) -> JSONResponse:
+    logger.info(f"Deleting {filepath} and no of files {len(filepath)}")
+    try:
+        if os.path.exists(filepath) and os.path.isfile(filepath):
+            os.remove(filepath)
+            logger.info(f"File {filepath} deleted successfully", exc_info=True)
+        else:
+            logger.info(f"File {filepath} not found", exc_info=True)
+        return JSONResponse(content={"message": "Files deleted successfully"}, status_code=200)
+    except Exception as e:
+        logger.error(f"Error deleting files: {e}", exc_info=True)
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+    finally:
+        gc.collect()
+
 
 if __name__ == "__main__":
     import uvicorn

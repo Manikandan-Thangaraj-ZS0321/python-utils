a32db6310c25a461cd98ed54d0dd879978e2f7f4
dineshkumar.anandan@zucisystems.com <dineshkumar.anandan@zucisystems.com>
2024-04-11 16:19:07 +0530

    refactor commit-3

--- app/multipartUtil.py
+++ app/multipartUtil.py
@@ -13,23 +13,24 @@ CONFIG = get__common__config()
 
 
 @app.post("/multipart-upload")
-async def create_papers(file: UploadFile = File(...), output_dir: str = Query(...)) -> dict:
+async def create_papers(file: UploadFile = File(...), outputDir: str = Query(...)) -> dict:
     try:
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir, exist_ok=True)
-        output_file = os.path.join(output_dir, file.filename)
+        if not os.path.exists(outputDir):
+            os.makedirs(outputDir, exist_ok=True)
+        output_file = os.path.join(outputDir, file.filename)
         if os.path.exists(output_file):
             logger.info("File {} already exists".format(file.filename))
             return {"message": "File already downloaded", "filename": file.filename, "filepath": output_file,
                     "status": "Success"}
         with open(output_file, "wb") as f:
             f.write(file.file.read())
+        print(file.filename)
         logger.info("File {} downloaded successfully".format(file.filename), exc_info=True)
         return {"message": "File downloaded successfully", "filename": file.filename, "filepath": output_file,
                 "status": "Success"}
     except Exception as e:
         logger.error("Error in downloading multipart file {} with exception {}".format(file.filename, e), exc_info=True)
-        return {"error": str(e)}
+        raise HTTPException(status_code=500, detail=str(e))
     finally:
         gc.collect()
 
@@ -52,21 +53,21 @@ async def do_upload_file(filepath: str) -> StreamingResponse:
                                  headers={"Content-Disposition": f"attachment; filename={filename}"})
     except Exception as ex:
         logger.error(f"Error in uploading multipart file {filepath} with exception {ex}", exc_info=True)
-        raise ex
+        raise HTTPException(status_code=500, detail=str(ex))
     finally:
         gc.collect()
 
 
-@app.post("/delete-file")
-async def delete_files(filepath: str) -> dict:
-    logger.info(f"Deleting {filepath} and no of files {len(filepath)}")
+@app.delete("/multipart-delete")
+async def delete_files(filepath: str = Query(...)) -> dict:
+    logger.info(f"Request to delete multipart file {filepath}")
     try:
         if os.path.exists(filepath) and os.path.isfile(filepath):
             os.remove(filepath)
             logger.info(f"File {filepath} deleted successfully", exc_info=True)
         else:
             logger.info(f"File {filepath} not found", exc_info=True)
-        return {"status": True, "message": "Files deleted successfully", "filepath": filepath}
+        return {"status": "Success", "message": "Files deleted successfully", "filepath": filepath}
     except Exception as e:
         logger.error(f"Error deleting files: {e}", exc_info=True)
         raise HTTPException(status_code=500, detail="Internal Server Error")
